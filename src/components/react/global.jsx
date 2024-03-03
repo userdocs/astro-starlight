70089ffa4990144625cc426cdaaf23b6318bd784
userdocs <16525024+userdocs@users.noreply.github.com>
2024-03-03 14:46:12 +0000

    docs

---
+++ src/components/react/global.jsx
@@ -0,0 +1,5 @@
+import Advanced from "/src/components/react/advancedMarkdown.jsx";
+import Details from "/src/components/react/details.jsx";
+import Tips from "/src/components/react/tooltip.jsx"
+
+export { Advanced, Details, Tips };
