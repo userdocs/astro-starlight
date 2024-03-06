e5dbbe494ff9b0f17ce92f9e311c42051969bf3d
userdocs <16525024+userdocs@users.noreply.github.com>
2024-03-06 00:42:42 +0000

    docs

--- src/components/global.jsx
+++ src/components/global.jsx
@@ -2,5 +2,6 @@ import Advanced from "/src/components/react/advancedMarkdown.jsx";
 import Details from "/src/components/details.astro";
 import Modal from '/src/components/modal.astro';
 import Tooltip from '/src/components/tooltip.astro';
+import Charts from "/src/components/charts.astro"
 
-export { Advanced, Details, Modal, Tooltip };
+export { Advanced, Details, Modal, Tooltip, Charts };
