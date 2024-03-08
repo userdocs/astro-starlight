7cc24bf9237da1955d1bd46124f27291372d0dd5
userdocs <16525024+userdocs@users.noreply.github.com>
2024-03-08 11:18:15 +0000

    Update global.jsx

--- src/components/global.jsx
+++ src/components/global.jsx
@@ -3,5 +3,6 @@ import Details from "/src/components/details.astro";
 import Modal from '/src/components/modal.astro';
 import Tooltip from '/src/components/tooltip.astro';
 import Charts from "/src/components/charts.astro"
+import Important from "/src/components/important.astro"
 
-export { Advanced, Details, Modal, Tooltip, Charts };
+export { Advanced, Details, Modal, Tooltip, Charts, Important };
